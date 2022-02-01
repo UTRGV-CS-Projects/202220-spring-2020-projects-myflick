67527940b4a908a8e8de8bb494e057e0c4d830a8
vvanessag <vanessathefirst@protonmail.com>
2022-02-01 13:11:18 -0600

    myProfile screen

---
+++ screens/MyProfile.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {ScrollView, StyleSheet, Text, View } from "react-native";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+
+export default function MyProfile(){
+
+    <SafeAreaView style={styles.container}>
+        <ScrollView showsVerticalScrollIndicator={false}> 
+        <View style={}> 
+            <View>
+
+            </View>
+        </View>
+
+
+        </ScrollView>
+    </SafeAreaView>
+}
+
+
+const styles= StyleSheet{
+    
+
+
+
+
+
+}
