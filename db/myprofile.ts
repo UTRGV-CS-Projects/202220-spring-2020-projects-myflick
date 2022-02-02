03d55c59d063ccd6680830df13e07a735733acae
vvanessag <vanessathefirst@protonmail.com>
2022-02-02 03:16:13 -0600

    added some icons and stuff

---
+++ db/myprofile.ts
@@ -0,0 +1,16 @@
+export interface ProfilePage {
+    id: string;
+    image: string;
+    hasPhoto: boolean;
+  }
+
+  export const MyProfilePicture: ProfilePage[] = [
+    {
+      id: "1",
+      image: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZSUyMHBpY3R1cmVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+      hasPhoto: true,
+
+  },
+    
+
+  ];
--- screens/MyProfile.tsx
+++ screens/MyProfile.tsx
@@ -1,26 +1,40 @@
-import React from "react";
-import { ScrollView, StyleSheet, Text, View, Image } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, StyleSheet, Text, View, Image, Pressable, TouchableOpacity } from "react-native";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { RootStackScreenProps } from "../types";
+import { setStatusBarBackgroundColor } from "expo-status-bar";
+import { MyProfilePicture, ProfilePage} from "../db/myprofile";
+//import { IconButton, Colors } from 'react-native-paper';
 
 const MyProfile = ({ navigation }: RootStackScreenProps<"MyProfile">) => {
-  return (
+    const [timesPressed, setTimesPressed] = useState(0);
+    return (
     <SafeAreaView style={styles.container}>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.titleBar}>
-          <View>
-            <Ionicons name="arrow-back" size={24} color="black"></Ionicons>
-            <Text></Text>
-            <Ionicons name="share-outline" size={24} color="black"></Ionicons>
+      <View style={styles.titleBar}>
+      <TouchableOpacity
+              onPress={() => {}}> 
+            <Ionicons name="arrow-back-sharp" size={30} color="black"></Ionicons></TouchableOpacity>
+            <Text style={styles.myName}>Gregory Jones</Text>
+            <TouchableOpacity
+              onPress={() => {}}> 
+            <Ionicons name="share-outline" size={30} color="black"></Ionicons></TouchableOpacity>
           </View>
-
-          <View style={{ alignContent: "center" }}>
+          <View style={{ alignSelf: "center" }}>
             <View style={styles.profileImage}>
-              {/*                     <Image source={require('./my-icon.png')} /> */}
+               <Image style={styles.image} source={{uri:"https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZSUyMHBpY3R1cmVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"}}></Image>
+            </View>
+            <View style={styles.add}>
+            <TouchableOpacity
+              onPress={() => {}}> 
+               <Ionicons name="add-circle" size= {30} color="#ff1493" style={{marginTop: 20, marginLeft: 32}}></Ionicons> 
+               </TouchableOpacity>
+                </View>
+            <View style={styles.bio}>
+                <Text></Text>
             </View>
           </View>
-        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -33,6 +47,45 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
   },
-  titleBar: {},
-  profileImage: {},
+  titleBar: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      marginTop: 7,
+      marginHorizontal: 16,
+    
+  },
+  profileImage: {
+      width: 100,
+      height: 100,
+      borderRadius: 50,
+      marginTop: 10,
+      overflow: "hidden"
+  },
+  image:{
+      flex: 1,
+      width: undefined,
+      height: undefined
+  },
+  add:{
+      position: "absolute",
+      bottom: 0,
+      right: 0,
+      width: 60,
+      height: 60,
+      borderRadius: 30,
+      alignItems: "center",
+      justifyContent: "center"
+  },
+  wrapperCustom:{
+    borderRadius: 8,
+    padding: 6,
+  },
+  myName:{
+      fontSize: 20,
+      fontWeight: "bold",
+  },
+  bio:{
+
+  },
+  
 });
