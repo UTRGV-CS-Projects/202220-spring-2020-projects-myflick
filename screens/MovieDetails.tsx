55f3e8f4ae148af6b63e08bb7ca1de5a21f846f3
Joseph B <joseph.balderas02@utrgv.edu>
2022-02-20 21:17:25 -0600

    Created a branch and MovieDetails screen

---
+++ screens/MovieDetails.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+function MovieDetails() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default MovieDetails;
+
+
+
+
+
