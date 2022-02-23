91b13b1d311e3e32a98d4a2d5804f8af9453c3bd
Jordan Molina <jordanmichaelmolina3@gmail.com>
2022-02-22 18:50:10 -0600

    update functinos

--- apis/movies.ts
+++ apis/movies.ts
@@ -4,18 +4,19 @@ const url = "https://api.themoviedb.org/3";
 
 const API_KEY = "2f7be12fe737d23f6891fca3cdd1b33f";
 
-export const fetchDiscovery = () =>
-  axios.get(
+export const fetchDiscovery = async () => {
+  const { data } = await axios.get(
     url +
       `/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=1&with_genres=true&with_watch_monetization_types=flatrate`
   );
-
-export const fetchSimilarMovies = (movieID: string) =>
-  axios.get(
+};
+export const fetchSimilarMovies = async (movieID: string) => {
+  const { data } = await axios.get(
     url + `/movie/${movieID}/similar?api_key=${API_KEY}&language=en-US&page=1`
   );
-
-export const fetchNowPlaying = () =>
-  axios.get(
+};
+export const fetchNowPlaying = async () => {
+  const { data } = await axios.get(
     url + `/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
   );
+};
