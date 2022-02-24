4a834816ad5855ac445a8f12af1630ab98b4ba6b
Jordan Molina <jordanmichaelmolina3@gmail.com>
2022-02-24 15:32:16 -0600

    amplify setup auth + api

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-
+import Amplify from "aws-amplify";
+import awsconfig from "./src/aws-exports";
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+Amplify.configure(awsconfig);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
---
+++ src/graphql/subscriptions.ts
@@ -0,0 +1,55 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateProfile = /* GraphQL */ `
+  subscription OnCreateProfile {
+    onCreateProfile {
+      id
+      name
+      bio
+      gender
+      age
+      images
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const onUpdateProfile = /* GraphQL */ `
+  subscription OnUpdateProfile {
+    onUpdateProfile {
+      id
+      name
+      bio
+      gender
+      age
+      images
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const onDeleteProfile = /* GraphQL */ `
+  subscription OnDeleteProfile {
+    onDeleteProfile {
+      id
+      name
+      bio
+      gender
+      age
+      images
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
