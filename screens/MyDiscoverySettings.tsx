181e2be29fba61b3f76d6bc2be59966eb42882b5
vvanessag <vanessathefirst@protonmail.com>
2022-03-16 02:50:20 -0500

    refactoring

--- screens/MyDiscoverySettings.tsx
+++ screens/MyDiscoverySettings.tsx
@@ -22,46 +22,8 @@ const MyDiscoverySettings = ({navigation,}: RootStackScreenProps<"MyDiscoverySet
 	const multiSliderValuesChange = (values: any) => setMultiSliderValue(values);
 	const [isEnabled, setIsEnabled] = useState(false);
 	const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
-	const [isEnabled2, setIsEnabled2] = useState(false);
-	const toggleSwitch2 = () => setIsEnabled2((previousState) => !previousState);
-	const [location, setLocation] = useState<any | null>(null);
-  	const [errorMsg, setErrorMsg] = useState<any | null>(null);
-
-
-	  /* useEffect(() => {
-		(async () => {
-		  if (Platform.OS === 'android' && !Constants.isDevice) {
-			setErrorMsg(
-			  'Oops, this will not work on Snack in an Android emulator. Try it on your device!'
-			);
-			return;
-		  }
-		  let { status } = await Location.requestForegroundPermissionsAsync();
-		  if (status !== 'granted') {
-			setErrorMsg('Permission to access location was denied');
-			return;
-		  }
-	
-		  let location = await Location.getCurrentPositionAsync();
-		  setLocation(location);
-
-		  
-
-		  
-		})();
-	  }, []);
-	
-	  let text = 'Waiting..';
-	  if (errorMsg) {
-		text = errorMsg;
-	  } else if (location) {
-		text = JSON.stringify(location);
-	  } */
-	
-	  const [locationServiceEnabled, setLocationServiceEnabled] = useState(false);
-	  const [displayCurrentAddress, setDisplayCurrentAddress] = useState(
-		'Loading...'
-	  );
+	const [locationServiceEnabled, setLocationServiceEnabled] = useState(false);
+	const [displayCurrentAddress, setDisplayCurrentAddress] = useState('Loading...');
 	
 	  useEffect(() => {
 		CheckIfLocationEnabled();
