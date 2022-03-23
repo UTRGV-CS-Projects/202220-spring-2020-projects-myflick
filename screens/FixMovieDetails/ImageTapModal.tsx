a2cfbc892299704d39d831cf34a754892c959498
Joseph B <joseph.balderas02@utrgv.edu>
2022-03-23 06:20:44 -0500

    Added a ImageTapModal File for tapping the image and opening a Modal on that image.

---
+++ screens/FixMovieDetails/ImageTapModal.tsx
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import { View, StyleSheet, Image, TouchableOpacity, Modal, TouchableHighlight, Text, Button } from 'react-native';
+
+import Screen from '../components/Screen';
+
+const image = { uri: "https://wallpaperaccess.com/full/1508305.jpg" };
+
+interface Props {
+  image ?: any
+}
+
+const ImageTapModal:React.FC<Props> = ({image}) => {
+
+    const [modalVisible, setModalVisible] = useState(false)
+
+
+  return (
+    <View style={styles.container}>
+
+        <TouchableOpacity activeOpacity={.5} onPress={() => setModalVisible(true)}>
+            <Image source={image} style={styles.image}/>
+        </TouchableOpacity>
+       
+    <Modal visible={modalVisible} animationType="fade" transparent={true} onRequestClose={() => setModalVisible(false)}>
+        <Screen style={styles.container}>
+        <TouchableOpacity style={styles.modalContainer} onPress={() => setModalVisible(false)}>
+             <TouchableOpacity style={styles.modal} onPress={() => console.log('Test')} activeOpacity={1} >
+                    <Image source={image} style={styles.imageModal} />
+                </TouchableOpacity>
+                </TouchableOpacity>
+        </Screen>
+    </Modal>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    zIndex: 1
+  },
+  image: {
+    height: 220,
+    width: 150,
+    borderRadius: 25,
+    left: 40,
+    top: 80,
+    // position: 'absolute',
+  },
+  imageModal: {
+    height: '100%',
+    width: '100%'
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modal: {
+    width: 155,
+    height: 300
+  },
+});
+
+export default ImageTapModal;
