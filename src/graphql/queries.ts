f60c17ca82ce026fda2867295947c85cc15f9d17
Jordan Molina <jordanmichaelmolina3@gmail.com>
2022-03-28 20:58:11 -0500

    user picture storage

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -378,3 +378,42 @@ export const listMessages = /* GraphQL */ `
     }
   }
 `;
+export const getPicture = /* GraphQL */ `
+  query GetPicture($id: ID!) {
+    getPicture(id: $id) {
+      id
+      name
+      owner
+      file {
+        bucket
+        region
+        key
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listPictures = /* GraphQL */ `
+  query ListPictures(
+    $filter: ModelPictureFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPictures(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        owner
+        file {
+          bucket
+          region
+          key
+        }
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
