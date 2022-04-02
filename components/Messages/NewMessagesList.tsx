736ea3588c9b4c88b1bdd1dc02e3115dbad6a3e3
Jordan Molina <jordanmichaelmolina3@gmail.com>
2022-04-01 21:45:39 -0500

    loading state

--- components/Messages/NewMessagesList.tsx
+++ components/Messages/NewMessagesList.tsx
@@ -1,4 +1,4 @@
-import { FlatList, StyleSheet } from "react-native";
+import { FlatList, RefreshControl, StyleSheet } from "react-native";
 import React, { useContext, useEffect, useRef, useState } from "react";
 import { View, Text } from "../Themed";
 import { People, PeopleDetailsType } from "../../db/db";
@@ -8,17 +8,21 @@ import { fetchUserConversations } from "../../apis/messages";
 import { AuthContext } from "../../store/AuthContext";
 import { Message as MessageType, UserConversations } from "../../src/API";
 import { ConversationType, MessageUser } from "../../types";
+import { themeColor } from "../../constants/Colors";
 
 interface NewMessagesListProps {
   navigationProp: any;
   conversations: ConversationType[];
+  setLoading: (loading: boolean) => void;
 }
 const NewMessagesList = ({
   navigationProp,
   conversations,
+  setLoading,
 }: NewMessagesListProps) => {
   const ref = useRef(null);
   useScrollToTop(ref);
+  const [refreshing, setRefreshing] = React.useState(false);
 
   useEffect(() => {}, []);
 
@@ -28,6 +32,16 @@ const NewMessagesList = ({
     <View style={styles.container}>
       <FlatList
         ref={ref}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            colors={[themeColor]}
+            tintColor={themeColor}
+            onRefresh={() => {
+              setLoading(true);
+            }}
+          />
+        }
         data={conversations}
         contentContainerStyle={styles.newMatchesListContainer}
         showsHorizontalScrollIndicator={false}
