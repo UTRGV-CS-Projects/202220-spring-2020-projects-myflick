a0dfd70adad36ee247e26cb35ca74866d8b86161
Joseph B <joseph.balderas02@utrgv.edu>
2022-04-05 21:55:58 -0500

    Test Example

--- components/__tests__/App-test.js
+++ components/__tests__/App-test.js
@@ -3,15 +3,30 @@ import React from "react";
 import renderer from 'react-test-renderer';
 import MoviePoster from '../MovieDetails/MoviePoster'
 import ImageTapModal from "../ImageTapModalFile/ImageTapModal";
+import {cleanup, render} from '@testing-library/react-native';
 import { FlatList, Text } from "react-native";
-import { render } from '@testing-library/react-native';
 const poster_path = "/qsdjk9oAKSQMWs0Vt5Pyfh6O4GZ.jpg";
 
-import App from "../../App";
+
 import Screen from '../Screen'
-describe.skip('<App />', () => {
+
+
+describe('Checking Screen Component to see if it has a child element', () => {
     it('has 1 child', () => {
       const tree = renderer.create(<Screen />).toJSON();
       expect(tree.children.length).toBe(1)
     });
   }); 
+
+describe.only('Checking the Image Tap Modal', () => {
+  it('Test function for the <ImageTapModa />', () => {
+    const testIdName = "touchableopac"
+
+    const {getByTestId} = render(<ImageTapModal />)
+    const foundTouchable = getByTestId(testIdName);
+
+    expect(foundTouchable).toBeTruthy();
+
+  })
+})
+
