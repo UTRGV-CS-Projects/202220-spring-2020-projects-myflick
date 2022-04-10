a7d5116bf71cd5ffb4d8fcb8720ed672547f3636
Ferb7o2 <71108160+JoseJaramillo04@users.noreply.github.com>
2022-04-10 01:46:07 -0500

    Created a graphql Query and method to retrieve data

--- apis/users.ts
+++ apis/users.ts
@@ -4,29 +4,29 @@ import { getUser, listUsers } from "../src/graphql/queries";
 import { GraphQLResult } from "@aws-amplify/api-graphql";
 
 export const fetchUsers = async () => {
-  try {
-    const data = (await API.graphql(
-      graphqlOperation(listUsers)
-    )) as GraphQLResult<ListUsersQuery>;
+	try {
+		const data = (await API.graphql(
+			graphqlOperation(listUsers)
+		)) as GraphQLResult<ListUsersQuery>;
 
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export const fetchUserDataMessage = async (id: string) => {
-  try {
-    const data = (await API.graphql(
-      graphqlOperation(getUserDataMessage, {
-        cognitoId: id,
-      })
-    )) as GraphQLResult<GetUserQuery>;
+	try {
+		const data = (await API.graphql(
+			graphqlOperation(getUserDataMessage, {
+				cognitoId: id,
+			})
+		)) as GraphQLResult<GetUserQuery>;
 
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 const getUserDataMessage = `query GetUser($cognitoId: ID!) {
@@ -36,3 +36,25 @@ const getUserDataMessage = `query GetUser($cognitoId: ID!) {
     cognitoId
   }
 }`;
+
+const getLikedInfo = `query MyQuery {
+  getLikedTable(likedId: $likedId) {
+    UserLikingId
+    VictimId
+    likedId
+  }
+}`;
+
+export const fetchLikedInfo = async (id: string) => {
+	try {
+		const data = (await API.graphql(
+			graphqlOperation(getLikedInfo, {
+				likedId: id,
+			})
+		)) as GraphQLResult<GetUserQuery>;
+
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+};
--- screens/Messages.tsx
+++ screens/Messages.tsx
@@ -4,183 +4,192 @@ import NewMatchesList from "../components/Messages/NewMatchesList";
 import NewMessagesList from "../components/Messages/NewMessagesList";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import {
-  ConversationType,
-  MessageUser,
-  RootStackScreenProps,
-  ErrorTypes,
+	ConversationType,
+	MessageUser,
+	RootStackScreenProps,
+	ErrorTypes,
 } from "../types";
 import { SafeAreaView, Text, View } from "../components/Themed";
 import {
-  fetchConversations,
-  fetchUserConversations,
-  listMessage,
+	fetchConversations,
+	fetchUserConversations,
+	listMessage,
 } from "../apis/messages";
 import { AuthContext } from "../store/AuthContext";
 import { Message, UserConversations } from "../src/API";
 import { listConversations } from "../src/graphql/queries";
-import { fetchUserDataMessage } from "../apis/users";
+import { fetchUserDataMessage, fetchLikedInfo } from "../apis/users";
 import { themeColor } from "../constants/Colors";
 
 const Messages = ({ navigation }: RootStackScreenProps<"Messages">) => {
-  const insets = useSafeAreaInsets();
-  const { user, dispatch } = useContext(AuthContext);
-  const [loading, setLoading] = useState(true);
-  const [conversations, setConversations] = useState<ConversationType[]>([]);
-  const conversationIdSet = useRef(new Set<string>());
-  //const [messages, setMessages] = useState<Message[][]>([]);
-  const getData = async () => {
-    console.log("getting data");
-    fetchUserConversations(user.cognitoId).then((res) => {
-      const userConversations = res?.data?.listUserConversations
-        ?.items as UserConversations[];
-      if (userConversations.length > 0) {
-        userConversations.forEach((userConversation) => {
-          let lastMessage = "";
-          let conversationMessages: Message[] = [];
-          let messageUser: MessageUser;
-          let messages: Message[];
+	const insets = useSafeAreaInsets();
+	const { user, dispatch } = useContext(AuthContext);
+	const [loading, setLoading] = useState(true);
+	const [conversations, setConversations] = useState<ConversationType[]>([]);
+	const conversationIdSet = useRef(new Set<string>());
+	//const [messages, setMessages] = useState<Message[][]>([]);
 
-          listMessage(userConversation.conversationId)
-            .then((res) => {
-              messages = res?.data?.listMessages?.items as Message[];
-              if (messages.length > 0) {
-                messages = messages.sort((a, b) => {
-                  return a.createdAt.localeCompare(b.createdAt);
-                });
-              }
-            })
-            .then(() => {
-              fetchConversations(userConversation?.conversationId).then(
-                (res) => {
-                  res?.data?.listConversations?.items?.map((conversation) => {
-                    let name: string;
-                    if (conversation?.name1 === user.cognitoId) {
-                      name = conversation!.name2;
-                    } else {
-                      name = conversation!.name1;
-                    }
-                    fetchUserDataMessage(name)
-                      .then((res) => {
-                        messageUser = res?.data?.getUser as MessageUser;
-                        if (messages.length > 0) {
-                          lastMessage = messages[messages.length - 1]?.content;
-                        } else {
-                          lastMessage = ErrorTypes.START_CONVERSATING;
-                        }
+	const getLikedData = async () => {
+		console.log("GETTING LIKED DATA *^*^*^*");
+		fetchLikedInfo("0").then((res) => {
+			console.log(res);
+		});
+	};
 
-                        return res;
-                      })
+	const getData = async () => {
+		console.log("getting data");
+		fetchUserConversations(user.cognitoId).then((res) => {
+			const userConversations = res?.data?.listUserConversations
+				?.items as UserConversations[];
+			if (userConversations.length > 0) {
+				userConversations.forEach((userConversation) => {
+					let lastMessage = "";
+					let conversationMessages: Message[] = [];
+					let messageUser: MessageUser;
+					let messages: Message[];
 
-                      .then(() => {
-                        const duplicated = conversationIdSet.current.has(
-                          userConversation.conversationId
-                        );
-                        if (duplicated) {
-                          const newConversations = conversations.map(function (
-                            item,
-                            i
-                          ) {
-                            if (
-                              item.conversationId ==
-                              userConversation.conversationId
-                            ) {
-                              return {
-                                conversationId:
-                                  userConversation!.conversationId,
-                                user: messageUser,
-                                lastMessage,
-                                messages,
-                              };
-                            } else {
-                              return item;
-                            }
-                          });
-                          setConversations(newConversations);
-                        } else {
-                          setConversations([
-                            ...conversations,
-                            {
-                              conversationId: userConversation!.conversationId,
-                              user: messageUser,
-                              lastMessage,
-                              messages,
-                            },
-                          ]);
-                          conversationIdSet.current.add(
-                            userConversation!.conversationId
-                          );
-                        }
-                      });
-                  });
-                }
-              );
-            });
-        });
-      } else {
-        setConversations([]);
-      }
-      setLoading(false);
-    });
-  };
-  useEffect(() => {
-    console.log("messages mounted");
-  }, []);
+					listMessage(userConversation.conversationId)
+						.then((res) => {
+							messages = res?.data?.listMessages?.items as Message[];
+							if (messages.length > 0) {
+								messages = messages.sort((a, b) => {
+									return a.createdAt.localeCompare(b.createdAt);
+								});
+							}
+						})
+						.then(() => {
+							fetchConversations(userConversation?.conversationId).then(
+								(res) => {
+									res?.data?.listConversations?.items?.map((conversation) => {
+										let name: string;
+										if (conversation?.name1 === user.cognitoId) {
+											name = conversation!.name2;
+										} else {
+											name = conversation!.name1;
+										}
+										fetchUserDataMessage(name)
+											.then((res) => {
+												messageUser = res?.data?.getUser as MessageUser;
+												if (messages.length > 0) {
+													lastMessage = messages[messages.length - 1]?.content;
+												} else {
+													lastMessage = ErrorTypes.START_CONVERSATING;
+												}
 
-  useEffect(() => {
-    console.log("conversations", conversations);
-  }, [conversations]);
+												return res;
+											})
 
-  useEffect(() => {
-    if (loading) {
-      getData();
-    }
-  }, [loading]);
+											.then(() => {
+												const duplicated = conversationIdSet.current.has(
+													userConversation.conversationId
+												);
+												if (duplicated) {
+													const newConversations = conversations.map(function (
+														item,
+														i
+													) {
+														if (
+															item.conversationId ==
+															userConversation.conversationId
+														) {
+															return {
+																conversationId:
+																	userConversation!.conversationId,
+																user: messageUser,
+																lastMessage,
+																messages,
+															};
+														} else {
+															return item;
+														}
+													});
+													setConversations(newConversations);
+												} else {
+													setConversations([
+														...conversations,
+														{
+															conversationId: userConversation!.conversationId,
+															user: messageUser,
+															lastMessage,
+															messages,
+														},
+													]);
+													conversationIdSet.current.add(
+														userConversation!.conversationId
+													);
+												}
+											});
+									});
+								}
+							);
+						});
+				});
+			} else {
+				setConversations([]);
+			}
+			setLoading(false);
+		});
+	};
+	useEffect(() => {
+		console.log("messages mounted");
+	}, []);
 
-  return (
-    <SafeAreaView style={[styles.container]}>
-      {loading ? (
-        <ActivityIndicator
-          animating={loading}
-          color={themeColor}
-          size={"large"}
-        />
-      ) : (
-        <>
-          <NewMatchesList
-            navigationProp={navigation}
-            conversations={conversations}
-          />
-          <NewMessagesList
-            navigationProp={navigation}
-            conversations={conversations}
-            setLoading={setLoading}
-          />
-        </>
-      )}
-    </SafeAreaView>
-  );
+	useEffect(() => {
+		console.log("conversations", conversations);
+	}, [conversations]);
+
+	useEffect(() => {
+		if (loading) {
+			getData();
+			getLikedData();
+		}
+	}, [loading]);
+
+	return (
+		<SafeAreaView style={[styles.container]}>
+			{loading ? (
+				<ActivityIndicator
+					animating={loading}
+					color={themeColor}
+					size={"large"}
+				/>
+			) : (
+				<>
+					<NewMatchesList
+						navigationProp={navigation}
+						conversations={conversations}
+					/>
+					<NewMessagesList
+						navigationProp={navigation}
+						conversations={conversations}
+						setLoading={setLoading}
+					/>
+				</>
+			)}
+		</SafeAreaView>
+	);
 };
 
 export default Messages;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  noMessages: {
-    color: themeColor,
-    fontSize: 22,
-  },
-  button: {
-    backgroundColor: themeColor,
-    padding: 12,
-    marginVertical: 10,
-    borderRadius: 10,
-    alignSelf: "center",
-  },
-  buttonText: {
-    color: "white",
-  },
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	noMessages: {
+		color: themeColor,
+		fontSize: 22,
+	},
+	button: {
+		backgroundColor: themeColor,
+		padding: 12,
+		marginVertical: 10,
+		borderRadius: 10,
+		alignSelf: "center",
+	},
+	buttonText: {
+		color: "white",
+	},
 });
