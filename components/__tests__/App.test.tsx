c46526e4df77ad5ef9307e34e57237f63598df13
Joseph B <joseph.balderas02@utrgv.edu>
2022-04-18 12:17:34 -0500

    Added the Signin.test.jsx file

--- components/__tests__/App.test.tsx
+++ components/__tests__/App.test.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import TestScreen from '../../screens/TestScreen'
 import {cleanup, fireEvent, render} from '@testing-library/react-native';
-// npm run test
+
+// type 'npm run test' in terminal to start
 
 describe("Testing the Test Screen", () => {
     it("Has 1 child View", () => {
-        const tree: any = renderer.create(<TestScreen />).toJSON();
-        expect(tree?.children?.length).toBe(2);
+        const tree: any = renderer.create(<TestScreen />).toJSON(); // Get/render the TestScreen component
+        expect(tree?.children?.length).toBe(2); // Assertion and tells either pass or fail
     })
     it("A button with testId Exists", () => {
         const testIdName = 'pressMeButton';
