1f461aca9fc03380a530d10f64f35e4dad89010e
Joseph B <joseph.balderas02@utrgv.edu>
2022-04-18 19:24:33 -0500

    Added MovieSwiping.test.tsx File.

---
+++ components/__tests__/MovieSwiping.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import MovieSwiping from '../../screens/MovieSwiping';
+import {cleanup, fireEvent, render} from '@testing-library/react-native'; // User actions.
+
+/*
+MovieSwiping
+ - Swiping Functionality
+ - Movie Click to get MovieDetails page
+*/
+
+const createTestProps = (props: any) => ({ 
+    navigation: {
+      navigate: jest.fn() // Mock the navigation.. No use for it.
+    },
+    ...props
+  })
+
+  describe('Testing the MovieSwiping Screen', () => {
+    describe("Rendering the MovieSwiping Screen", () =>{
+        let props: any;
+        let testId: any;
+        beforeEach(() => {
+          props = createTestProps({});
+          const {getAllByTestId} = render(<MovieSwiping {...props}/>)
+          testId = getAllByTestId;
+        })
+    it('Testing the swiping Functionality', () => {
+
+    })
+    it("Test the Movie Swiping image click to navigate to MovieDetails", () => {
+
+    })
+})
+})
