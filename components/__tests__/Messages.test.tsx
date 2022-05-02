3e0d7d3c0fd9090a0fd8405bb5ad28b0cf1cffbf
vvanessag <vanessathefirst@protonmail.com>
2022-05-02 01:24:32 -0500

    messaages test

--- components/__tests__/Messages.test.tsx
+++ components/__tests__/Messages.test.tsx
@@ -1,7 +1,11 @@
 import React, { useContext } from "react";
 import renderer from "react-test-renderer";
-import OpenChat from "../../screens/OpenChat";
+import OpenChat from "../profileComponents/openChatMock";
 import { fireEvent, render } from "@testing-library/react-native";
+import Message from "../Messages/Message";
+import Openmessage from "../profileComponents/Openmessage";
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+//jest.mock('NativeAnimatedHelp');
 
 // MyProfile Screen
 //  - Check buttons work
@@ -19,15 +23,38 @@ const createTestProps = (props: any) => ({
         let testId: any;
         beforeEach(() => {
           props = createTestProps({});
-          const {getAllByTestId} = render(<OpenChat {...props}/>);
+          const {getAllByTestId} = render(<Openmessage {...props}  />);
           testId = getAllByTestId;})
-        it('User can type a message', async () => {
+        it('User can open a message', async () => {
+            const testIDName = "openmessage";
+            const open = testId(testIDName);
+            expect(open).toBeTruthy();
+
+        })
+        
+    })
+  })
+
+  describe('Testing the OpenChat Screen', () => {
+    describe("Rendering the OpenChat Screen", () =>{
+        let props: any;
+        let testId: any;
+        beforeEach(() => {
+          props = createTestProps({});
+          const {getAllByTestId} = render(<OpenChat {...props}  />);
+          testId = getAllByTestId;})
+        it('User can send a message', async () => {
             const testIDName = "sendbutton";
-            const foundButton = testId(testIDName);
-            expect(foundButton).toBeTruthy();
+            const send = testId(testIDName);
+            expect(send).toBeTruthy();
 
-
-
+        })
+        it('User can type a message', async () => {
+          const test = "typing";
+          const {getByTestId} = render(<OpenChat {...props}  />);
+          const text = getByTestId(test);
+          fireEvent.changeText(text, "typing");
+          expect(text.props.value).toBe("typing");
         })
     })
   })
